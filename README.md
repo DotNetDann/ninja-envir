09f3d0fae89522880638fc992a4b35ea0b7d6ebb
Daniel Hyles <daniel.hyles@gmail.com>
2013-04-07 13:55:41 +1000

    Now a working version

--- README.md
+++ README.md
@@ -4,20 +4,17 @@ Current Cost EnviR Power Meter
 ## Usage
 ```
 This Ninja Block module is installed on your ninja block (http://www.ninjablocks.com/)
-It will read the serial commands from your Current Cost EnviR power meter's serial port (http://www.currentcost.com/product-envir.html)
+It will read the serial commands from your Current Cost EnviR power meter's serial port
+You can use the EnviR's serial cable(http://www.currentcost.com/product-envir.html) or use the on-board beagle serial
 It will upload your current usage every 6 seconds when the unit sends data.
 This will then show in your Ninja Block dashboard and you will be able to make rules based upon your household power usage
+You also have the option to send the data to Cosm (replicating the bridge device)
 ```
 
 ## Installation
 ```
-1) Download the zip and extract to /opt/ninja/node_modules on your ninja block
-2) Get your serial ports name by: 
-	$ dmesg | grep tty 
-3) Edit index.js and update the serialPortName variable
-4) Don't know as I don't have node version 8 on my NB yet...
-
+cd /opt/ninja/drivers/
+rm -rf ninja-envir
+git clone https://github.com/DotNetDann/ninja-envir.git ninja-envir
+sudo service ninjablock restart
 ```
-
-
-
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Device = require('./lib/envir')
+  , Comms = require('./lib/comms')
   , util = require('util')
   , stream = require('stream');
 
@@ -19,23 +20,51 @@ util.inherits(envir,stream);
  * @fires register - Emit this when you wish to register a device (see Device)
  * @fires config - Emit this when you wish to send config data back to the cloud
  */
+ 
+
+/**
+ * Find your serial port via command line 
+ *    $ dmesg | grep tty
+ *
+ * Serial port name 'ttyUSB0' is for the USB cable - http://www.currentcost.com/product-datacable.html
+ * Serial port name 'ttyO5' is on-board Serial 5
+ * 		NOTE: You need to put these lines in /etc/rc.local
+ * 			echo 4 > /sys/kernel/debug/omap_mux/lcd_data8
+ * 			echo 24 > /sys/kernel/debug/omap_mux/lcd_data9
+ *
+ * 		CC128		Beagle Bone			Cat5 Wire Colour 	Notes
+ * 		---------------------------------------------------------------------------------
+ * 		Pin 1		Pin P9-01 or 02  	Blue				Gnd
+ * 		Pin 4		Pin P9-03 or 04 	Green Stripe		3.3v
+ * 		Pin 7		Pin P8-37			Brown Stripe		Serial Data (Optional)
+ * 		Pin 8		Pin P8-38			Brown				Serial Data
+ */
+
+ 
 function envir(opts,app) {
-
-  /**
-  * Check that the serial port is correct 
-  * $ dmesg | grep tty
-  */
-  var serialPortName = "ttyUSB0";
-
-
-// TODO: Check Serial ports. If none then exit.
-
-
-
+  this._app = app;
+  this._opts = opts;
   var self = this;
+
+  var currentCostComms = null;
+
+  if (self._opts.serialPortName == null) {
+    // Set the default varables
+    self._opts.serialPortName = "ttyUSB0";
+    self._opts.cosmSend = false;
+    self._opts.cosmSendMins = 5; // Send every 5 mins. Updates come in every 6 seconds.
+    self._opts.cosmFeedId = "";  
+    self._opts.cosmApiKey = "";  
+    self.save();
+  }
+  
 
   app.on('client::up',function(){
     // The client is now connected to the cloud
+
+    if (currentCostComms == null) {
+      currentCostComms = new Comms("/dev/" + self._opts.serialPortName);
+    }
 
     // Do stuff with opts, and then commit it to disk
     if (!opts.hasMutated) {
@@ -46,7 +75,7 @@ function envir(opts,app) {
 
     // Register a device
     console.log("Envir: Register");
-    self.emit('register', new Device(serialPortName));
+    self.emit('register', new Device(self._opts, currentCostComms));
   });
 };
 
